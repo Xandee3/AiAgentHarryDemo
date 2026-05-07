a22630060622b57331369864ce6529056db9543c
Dirk Sandhorst <sandhorst@inforce.de>
2026-05-07 10:37:07 +0200

    Initial commit

---
+++ AIAgentHarryDemo/ContactDialog.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace AIAgentHarryDemo
+{
+    public partial class ContactDialog : Window
+    {
+        public ContactDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
